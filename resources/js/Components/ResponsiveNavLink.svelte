df81af848085a0864329b997788d80782b9126c4
Takaya <gifu.u.yamamoto@gmail.com>
2023-08-19 05:16:42 +0900

    fix Responsive Droppdown Logout button

--- resources/js/Components/ResponsiveNavLink.svelte
+++ resources/js/Components/ResponsiveNavLink.svelte
@@ -1,7 +1,9 @@
 <script>
     import { Link } from "@inertiajs/svelte";
 
-    export let href = null,
+    export let href,
+        method = "get",
+        as = "button",
         active = false;
 
     const classes = active
@@ -9,6 +11,6 @@
         : "block w-full pl-3 pr-4 py-2 border-l-4 border-transparent text-left text-base font-medium text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 focus:outline-none focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700 focus:border-gray-300 dark:focus:border-gray-600 transition duration-150 ease-in-out";
 </script>
 
-<Link {href} class={classes}>
+<Link {as} {method} {href} class={classes}>
     <slot />
 </Link>
